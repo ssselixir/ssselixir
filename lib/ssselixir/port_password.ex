3658ead56ba736a5350446e5687b42c24652e5d0
SSSelixir <ssselixir@gmail.com>
2017-10-31 09:13:37 +0000

    Add create function for PortPassword to simplify record creation flow

--- lib/ssselixir/port_password.ex
+++ lib/ssselixir/port_password.ex
@@ -1,5 +1,7 @@
 defmodule SSSelixir.PortPassword do
   if Mix.Project.config[:pp_store] == :db do
+    alias SSSelixir.Crypto
+
     use Ecto.Schema
 
     schema "port_passwords" do
@@ -8,9 +10,17 @@ defmodule SSSelixir.PortPassword do
     end
 
     def changeset(record, params \\ %{}) do
+      # Encrypt password before saving
+      params = %{params | password: Crypto.gen_base64_encoded_key(params[:password])}
       record
       |> Ecto.Changeset.cast(params, [:port, :password])
       |> Ecto.Changeset.validate_required([:port, :password])
+    end
+
+    def create(%{port: _port, password: _password}=params) do
+      %SSSelixir.PortPassword{}
+      |> changeset(params)
+      |> SSSelixir.Repo.insert
     end
   end
 end
